dcf656b0caee879cdd92172095d63ae9bff47a31
Milan Sulc <felix@noblexity.com>
2012-08-15 16:33:37 +0200

    PropertyChangeManager added, in dev version

---
+++ src/com/nxj/utils/PropertyChangeManager.java
@@ -0,0 +1,78 @@
+package com.nxj.utils;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.logging.Logger;
+
+/**
+ * Copyright 2012 Noblexity Advertising
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+/**
+ * @author Petr Stuchl4n3k Stuchlik <stuchl4n3k@gmail.com>
+ * @author Milan Felix Sulc <rkfelix@gmail.com>
+ * 
+ * @nxj 0.1
+ * @version 1.0
+ */
+public class PropertyChangeManager {
+
+    private transient PropertyChangeSupport changeSupport;
+    private static final Logger logger = Logger.getLogger(PropertyChangeManager.class.getName());
+
+    public PropertyChangeManager() {
+        changeSupport = new PropertyChangeSupport(this);
+    }
+
+    /**
+     * Fire property change listener
+     * @param propertyName
+     * @param oldValue
+     * @param newValue
+     */
+    public void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+        if ((oldValue == null && newValue == null) || oldValue == newValue) {
+            return;
+        }
+        changeSupport.firePropertyChange(new PropertyChangeEvent(this, propertyName, oldValue, newValue));
+    }
+
+    /**
+     * Adds a property change listener width String key
+     * @param propertyName
+     * @param listener
+     */
+    public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(propertyName, listener);
+    }
+
+    /**
+     * Adds a property change listener
+     * @param listener
+     */
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    /**
+     * Removes the property change listner by propertyName
+     * @param propertyName
+     * @param listener
+     */
+    public void removePropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(propertyName, listener);
+    }
+}
